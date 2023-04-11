1367ea1f5b5d611e8ce58dd8ccca01f425a3326e
Marcus Hemmen <mhemmen93@gmail.com>
2023-04-11 14:00:56 +0200

    init

---
+++ index.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    scroll();
+    $(window).scroll(scroll);
+
+    function scroll(){
+        $('.content-2 .entry, .content-3 .entry').each( function(i){
+
+            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+            /* If the object is completely visible in the window, fade it it */
+            if( bottom_of_window > bottom_of_object ){
+                $(this).addClass('active');
+            } else {
+                $(this).removeClass('active');
+            }
+
+        });
+    }
+
+});
